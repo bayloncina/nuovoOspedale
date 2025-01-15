663ac19334e7d3f05afd641730836922e5a36791
bayloncina <bayloncina@hotmail.it>
2025-01-15 19:12:00 +0100

    primo commit

---
+++ ospedaleNuovo/src/main/java/com/example/ospedaleNuovo/OspedaleNuovoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ospedaleNuovo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OspedaleNuovoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OspedaleNuovoApplication.class, args);
+	}
+
+}
---
+++ ospedaleNuovo/src/test/java/com/example/ospedaleNuovo/OspedaleNuovoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ospedaleNuovo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OspedaleNuovoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
